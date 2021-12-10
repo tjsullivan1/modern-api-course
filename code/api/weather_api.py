911d5afdc769599eee345a0ef5b6764ec7f910a6
Tim Sullivan <timothyj.sullivan1@gmail.com>
2021-12-10 10:47:48 -0600

    Initial verison of our API

---
+++ code/api/weather_api.py
@@ -0,0 +1,15 @@
+from os import stat
+from typing import Optional
+import fastapi
+from fastapi import Depends
+from models.location import Location
+from services import openweather_service
+
+router = fastapi.APIRouter()
+
+@router.get('/api/weather/{city}')
+def weather(loc: Location = Depends(), units: Optional[str] = 'metric'):
+
+    report = openweather_service.get_report(loc.city, loc.state, loc.country, units)
+
+    return f"the report for {loc.city}, {loc.state}, {loc.country} in {units}:\n{report}"
