a2b4c7c39348fb474feb260a985a9272c67c7b53
Tim Sullivan <timothyj.sullivan1@gmail.com>
2021-12-10 14:42:51 -0600

    finished chapter 5

--- code/api/weather_api.py
+++ code/api/weather_api.py
@@ -8,8 +8,8 @@ from services import openweather_service
 router = fastapi.APIRouter()
 
 @router.get('/api/weather/{city}')
-def weather(loc: Location = Depends(), units: Optional[str] = 'metric'):
+async def weather(loc: Location = Depends(), units: Optional[str] = 'metric'):
 
-    report = openweather_service.get_report(loc.city, loc.state, loc.country, units)
+    report = await openweather_service.get_report_async(loc.city, loc.state, loc.country, units)
 
     return f"the report for {loc.city}, {loc.state}, {loc.country} in {units}:\n{report}"
--- code/services/openweather_service.py
+++ code/services/openweather_service.py
@@ -1,9 +1,21 @@
 from typing import Optional
+import httpx
 
 api_key: Optional[str] = None
 
-def get_report(city: str, state: Optional[str], country: str, units: str) -> dict:
-    query = f'{city},{country}'
-    url = f'http://api.openweathermap.org/data/2.5/weather?q={query}&APPID={api_key}'
+async def get_report_async(city: str, state: Optional[str], country: str, units: str) -> dict:
+    if state:
+        query = f'{city},{state},{country}'
+    else:    
+        query = f'{city},{country}'
+        
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={query}&APPID={api_key}&units={units}'
+
+    async with httpx.AsyncClient() as client:
+        resp = await client.get(url)
+        resp.raise_for_status()
+
+    data = resp.json()
+    forecast = data['main']
     
-    return url
+    return forecast
