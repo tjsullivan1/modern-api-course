d5beefaf08b357ce3f962fac522cdcb6a15af3ab
Tim Sullivan <timothyj.sullivan1@gmail.com>
2021-12-16 13:37:07 -0600

    created sample small CLI app

---
+++ code/bin/reportapp.py
@@ -0,0 +1,48 @@
+import requests
+
+def main():
+    choice = input("[R]eport weather or [s]ee reports?")
+    while choice:
+        if choice.lower().strip() == 'r':
+            report_event()
+        elif choice.lower().strip() == 's':
+            see_events()
+        else:
+            print(f"I don't know what to do with {choice}")
+
+        choice = input("[R]eport weather or [s]ee reports?")
+
+
+def report_event():
+    desc = input("What is happening now?")
+    city = input("What city are you in?")
+    state = input("What is your state?")
+    data = {
+        "description": desc,
+        "location": {
+            "city": city,
+            "state": state
+        }
+    }
+
+    url = "http://127.0.0.1:8000/api/reports"
+    resp = requests.post(url, json=data)
+    resp.raise_for_status()
+
+    result = resp.json()
+
+    print(f"Reported new event: {result}")
+
+
+def see_events():
+    url = "http://127.0.0.1:8000/api/reports"
+    resp = requests.get(url)
+    resp.raise_for_status()
+
+    data = resp.json()
+    for r in data:
+        print(f"{r.get('location').get('city')} has {r.get('description')}")
+
+
+if __name__ == '__main__':
+    main()
